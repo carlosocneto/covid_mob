20cb99842504abeea03d7094135c8dee5df2365f
carlosocneto <carlos.o.c.neto@gmail.com>
2023-07-10 23:46:38 -0300

    correlação com regressão não paramétrica

--- teste/verifica_empresas_nas_listas.py
+++ teste/verifica_empresas_nas_listas.py
@@ -1,7 +1,45 @@
-import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import matplotlib.pyplot as plt
+sns.set_style("white")
+plt.rc("axes.spines", top=False, right=False)
+sns.set_context("paper")
 
-df_2011 = pd.read_csv('/home/carlos/Downloads/sarah/2011_clean.csv')
+# make some data
+x = 5*np.random.random(100)
+y = np.sin(x) * 3*np.exp(-x) + np.random.normal(0, 0.2, 100)
+plt.plot(x, y, 'k.')
 
-df_2012_2014 = pd.read_csv('/home/carlos/Downloads/sarah/2012-2014_clean.csv')
+from statsmodels.nonparametric.smoothers_lowess import lowess as  sm_lowess
+sm_x, sm_y = sm_lowess(y, x,  frac=1./5., 
+                           it=5, return_sorted = True).T
 
-pd.merge(df_2011, df_2012_2014, how='inner', on=['cnpj','regiao']).to_csv('/home/carlos/Downloads/2011_2014.csv',encoding='ISO-8859-1',index=False,sep=',')
+
+import scipy.interpolate
+
+def smooth(x, y, xgrid):
+    samples = np.random.choice(len(x), 50, replace=True)
+    y_s = y[samples]
+    x_s = x[samples]
+    y_sm = sm_lowess(y_s,x_s, frac=1./5., it=5,
+                     return_sorted = False)
+    # regularly sample it onto the grid
+    y_grid = scipy.interpolate.interp1d(x_s, y_sm, 
+                                        fill_value='extrapolate')(xgrid)
+    return y_grid
+
+xgrid = np.linspace(x.min(),x.max())
+K = 100
+smooths = np.stack([smooth(x, y, xgrid) for k in range(K)]).T
+
+mean = np.nanmean(smooths, axis=1)
+stderr = scipy.stats.sem(smooths, axis=1)
+stderr = np.nanstd(smooths, axis=1, ddof=0)
+# plot it
+plt.fill_between(xgrid, mean-1.96*stderr,
+                     mean+1.96*stderr, alpha=0.25)
+plt.plot(xgrid, mean, color='tomato')
+plt.plot(x, y, 'k.')
+
+plt.show()
